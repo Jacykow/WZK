d0f7d2dfeabd5fd58ffcb5529dd479ca003e10ad
Jacykow <jacgulij@gmail.com>
2020-10-24 15:56:53 +0200

    init

---
+++ Assets/Scripts/InputFieldController.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UniRx;
+using UnityEngine;
+
+[SelectionBase]
+public class InputFieldController : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField inputField;
+    [SerializeField]
+    private TextMeshProUGUI descriptionText;
+
+    public ReactiveProperty<string> InputProperty { get; } = new ReactiveProperty<string>();
+    public string Description { get => descriptionText.text; set => descriptionText.text = value; }
+
+    private void Start()
+    {
+        InputProperty.Value = inputField.text;
+        inputField.onEndEdit.AddListener(value => InputProperty.Value = value);
+    }
+}
